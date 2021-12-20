814ce497989d6c76bc82f31ad7e88c341f4a4d61
Ethan Barrance <theclassicsponge@gmail.com>
2021-12-20 21:46:18 +0000

    added new plots

--- Jan_2022/Midterm_Assessment_2020.r
+++ Jan_2022/Midterm_Assessment_2020.r
@@ -115,7 +115,7 @@ gg_vdem + geom_histogram()
 
 #### RECODES FOR VARIBELS OF INTREST (BINARY)####
 
-# calculate median population density
+# calculate median GDP Per Capita
 med_gdp <- median(dataset_omit$gdp_per_capita)
 
 # create new variable using case_when()
@@ -126,11 +126,35 @@ dataset_omit <- dataset_omit %>% # pipe the dataset
         gdp_per_capita > med_gdp ~ 1,
         gdp_per_capita <= med_gdp ~ 0)))
 
+# calculate median 65 or Older
+med_65_plus <- median(dataset_omit$aged_65_older)
+
+# create new variable using case_when()
+dataset_omit <- dataset_omit %>% # pipe the dataset
+  mutate( # create new variable
+    sixty_five_var = # name the new variable
+      factor(case_when(
+        aged_65_older > med_65_plus ~ 1,
+        aged_65_older <= med_65_plus ~ 0)))
+
+# calculate median vdem Polyarchy
+med_vdem <- median(dataset_omit$vdem_polyarchy)
+
+# create new variable using case_when()
+dataset_omit <- dataset_omit %>% # pipe the dataset
+  mutate( # create new variable
+    vdem_var = # name the new variable
+      factor(case_when(
+        vdem_polyarchy > med_vdem ~ 1,
+        vdem_polyarchy <= med_vdem ~ 0)))
+
 
 
 #### RELATIONSHP BETWEEN TOTAL COVID 19 DEATHS AND THE RELEVENT VARIABLES ####
 
   #### CONDITIONAL DISTRIBUTION PLOTS ####
+
+# Distribution of Deaths per Million Conditional on GDP per Capita
 
 gdp_dist <- ggplot(data = dataset_omit, aes(total_deaths_per_million, group = gdp_var)) + 
   geom_density(aes(colour = gdp_var)) +
@@ -144,4 +168,56 @@ gdp_dist <- ggplot(data = dataset_omit, aes(total_deaths_per_million, group = gd
 
 gdp_dist
 
+# Distribution of Deaths per Million Conditional on median 65 or Older
+
+sixtyfive_dist <- ggplot(data = dataset_omit, aes(aged_65_older, group = sixty_five_var)) + 
+  geom_density(aes(colour = sixty_five_var)) +
+  labs(x = "Total Deaths Per Million", # clearer x axis label
+       y = "Density", # clearer y axis label
+       title = "Distribution of Deaths per Million Conditional on the Estimate Share of the Population aged 65 or Older") + # title 
+  scale_color_discrete(name = "Over aged 65", # change legend title
+                       labels = c("Below or equal median", # change legend labels
+                                  "Above median")) + 
+  theme_minimal()
+
+sixtyfive_dist
+
+# Distribution of Deaths per Million Conditional on median vdem Polyarchy
+
+vdem_dist <- ggplot(data = dataset_omit, aes(vdem_polyarchy, group = vdem_var)) + 
+  geom_density(aes(colour = vdem_var)) +
+  labs(x = "Total Deaths Per Million", # clearer x axis label
+       y = "Density", # clearer y axis label
+       title = "Distribution of Deaths per Million Conditional on the Level of Liberal Democracy") + # title 
+  scale_color_discrete(name = "Level of Liberal Democracy", # change legend title
+                       labels = c("Below or equal median", # change legend labels
+                                  "Above median")) + 
+  theme_minimal()
+
+vdem_dist
+
   #### TWO SAMPLE T-TESTS ####
+
+# T-Test: GDP
+
+t.test(vdem_polyarchy ~ vdem_var, # formula y ~ x
+       data = dataset_omit, # dataset where the variables are found
+       mu = 0, # difference under the null hypothesis
+       alt = "two.sided",  # two sided test (difference in means could be smaller or larger than 0)
+       conf = 0.95) # confidence interval
+
+# T-Test: Aged 65 Plus
+
+t.test(aged_65_older ~ sixty_five_var, # formula y ~ x
+       data = dataset_omit, # dataset where the variables are found
+       mu = 0, # difference under the null hypothesis
+       alt = "two.sided",  # two sided test (difference in means could be smaller or larger than 0)
+       conf = 0.95) # confidence interval
+
+# T-Test: VDEM Polyarchy
+
+t.test(vdem_polyarchy ~ vdem_var, # formula y ~ x
+       data = dataset_omit, # dataset where the variables are found
+       mu = 0, # difference under the null hypothesis
+       alt = "two.sided",  # two sided test (difference in means could be smaller or larger than 0)
+       conf = 0.95) # confidence interval
