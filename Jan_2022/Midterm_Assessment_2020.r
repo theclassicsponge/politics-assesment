6368ea29e2a5a6067beedb35f3ec14ca7fd9b548
Ethan Barrance <theclassicsponge@gmail.com>
2021-12-22 19:20:18 +0000

    more work on writing

--- Jan_2022/Midterm_Assessment_2020.r
+++ Jan_2022/Midterm_Assessment_2020.r
@@ -88,16 +88,19 @@ sd(dataset_omit$vdem_polyarchy)
 #### A RELEVENT PLOT FOR EVERY VARIABLE ####
 
 # Total Deaths Per Million:
-gg_total_deaths_per_mil <- ggplot(dataset_omit, aes(x = total_deaths_per_million))
+gg_total_deaths_per_mil <- ggplot(dataset_omit, aes(x = total_deaths_per_million)) +
+  ggtitle("The Total Number of COVID-19 Related Deaths per Million Inhabitants")
 gg_total_deaths_per_mil + geom_histogram(binwidth = 10)
 
 # GDP Per Capita:
-gg_gdp_per_cap <- ggplot(dataset_omit, aes(x = gdp_per_capita))
+gg_gdp_per_cap <- ggplot(dataset_omit, aes(x = gdp_per_capita)) +
+  ggtitle("Gross Domestic Product (GDP) per capita")
 gg_gdp_per_cap + geom_histogram()
 
 # Female Head of Government:
 gg_female_HoG_deaths <- ggplot(data = dataset_omit, aes(x = female_HoG)) +
-  geom_histogram(binwidth = 20)
+  geom_histogram(binwidth = 20) + 
+  ggtitle("The Number of Female Heads of Government in the World (0 = Male, 1 = Female)")
 
 geom_text(size = 2, 
           colour = "red", 
@@ -106,11 +109,13 @@ geom_text(size = 2,
 gg_female_HoG_deaths + facet_wrap(~ female_HoG)
 
 # Aged 65 or Older:
-gg_aged_65_plus <- ggplot(data = dataset_omit, aes(x = aged_65_older))
+gg_aged_65_plus <- ggplot(data = dataset_omit, aes(x = aged_65_older)) + 
+  ggtitle("Estimated Share of the Population Aged 65 and Over")
 gg_aged_65_plus + geom_histogram()
 
 # VDEM Polyarchy:
-gg_vdem <- ggplot(data = dataset_omit, aes(x = vdem_polyarchy))
+gg_vdem <- ggplot(data = dataset_omit, aes(x = vdem_polyarchy)) + 
+  ggtitle("Level of Liberal Democracy, Negative Values Represent Lower Levels of Democracy, Higher Values Represent Higher Levels")
 gg_vdem + geom_histogram()
 
 #### RECODES FOR VARIBELS OF INTREST (BINARY)####
